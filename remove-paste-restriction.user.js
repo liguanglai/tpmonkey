d59fd7cea6ec516184fb1fc9221412cc536310f6
Li　Guanglai <liguanglai@bridge.vc>
2023-08-11 11:10:31 +0900

    Update remove-paste-restriction.user.js

--- remove-paste-restriction.user.js
+++ remove-paste-restriction.user.js
@@ -1,22 +1,27 @@
 // ==UserScript==
 // @name         移除禁止粘贴属性
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  移除指定输入框的不允许粘贴属性
+// @version      0.3
+// @description  在页面加载完成后移除指定输入框的不允许粘贴属性
 // @author       You
-// @match        https://target-website.com/*  // 这里替换为你的目标网站
+// @match        https://brain.hankyu-travel.com/*  // 这里替换为你的目标网站
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // 根据ID选择目标输入框
-    var targetInput = document.getElementById('txt_kosuno_pd0');
+    function removePasteRestriction() {
+        // 根据ID选择目标输入框
+        var targetInput = document.getElementById('txt_kosuno_pd0');
 
-    // 移除不允许粘贴的属性
-    if(targetInput) {
-        targetInput.removeAttribute('onpaste');
+        // 移除不允许粘贴的属性
+        if(targetInput) {
+            targetInput.removeAttribute('onpaste');
+        }
     }
+
+    // 使用 'DOMContentLoaded' 事件确保页面内容加载完成后再执行
+    document.addEventListener('DOMContentLoaded', removePasteRestriction);
 
 })();
