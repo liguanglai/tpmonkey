db205c70b113c338be789f83d99a291898ac60ae
Li　Guanglai <liguanglai@bridge.vc>
2023-08-11 11:07:46 +0900

    Update remove-paste-restriction.user.js

--- remove-paste-restriction.user.js
+++ remove-paste-restriction.user.js
@@ -4,15 +4,15 @@
 // @version      0.1
 // @description  移除指定输入框的不允许粘贴属性
 // @author       You
-// @match        https://brain.hankyu-travel.com/*  // 这里替换为你的目标网站
+// @match        https://target-website.com/*  // 这里替换为你的目标网站
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // 选择目标输入框，可以根据需要调整选择器
-    var targetInput = document.querySelector('form input[disabled]');  // 这只是一个例子，你应该替换为精确的选择器
+    // 根据ID选择目标输入框
+    var targetInput = document.getElementById('txt_kosuno_pd0');
 
     // 移除不允许粘贴的属性
     if(targetInput) {
